a60a4d5eb84a0a25cf1baea1d6c467af6474b069
Balmukund SINGH <balmukund.singh241@gmail.com>
2020-05-17 17:55:09 +0530

    Section-6/Assert Timeout with supplier message done.

--- src/test/java/com/crud/example/AssertTimeOutMethod.java
+++ src/test/java/com/crud/example/AssertTimeOutMethod.java
@@ -1,13 +1,15 @@
 package com.crud.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
+import com.crud.model.BookModel;
+import com.crud.service.BookService;
+import org.junit.jupiter.api.Test;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import com.crud.model.BookModel;
-import com.crud.service.BookService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class AssertTimeOutMethod {
 
@@ -27,13 +29,26 @@ class AssertTimeOutMethod {
 	@Test
 	void assert_timeout_with_messages() {
 		BookService service = new BookService();
-		for(int i=1; i<=10000; i++) {
-			service.addBookInList(new BookModel(String.valueOf(i),"Core Java","TechMax" ));
+		for (int i = 1; i <= 10000; i++) {
+			service.addBookInList(new BookModel(String.valueOf(i), "Core Java", "TechMax"));
 		}
 		List<String> actualTitles = new ArrayList<>();
-		assertTimeout(Duration.ofMillis(1), ()-> {
+		assertTimeout(Duration.ofMillis(1), () -> {
 			actualTitles.addAll(service.getBookTitlesByPublisher("TechMax"));
-		},"Performance issues with getBookTitlesByPublisher() method !");
+		}, "Performance issues with getBookTitlesByPublisher() method !");
+		assertEquals(10000, actualTitles.size());
+	}
+
+	@Test
+	void assert_timeout_with_supplier_messages() {
+		BookService service = new BookService();
+		for (int i = 1; i <= 10000; i++) {
+			service.addBookInList(new BookModel(String.valueOf(i), "Core Java", "TechMax"));
+		}
+		List<String> actualTitles = new ArrayList<>();
+		assertTimeout(Duration.ofMillis(1), () -> {
+			actualTitles.addAll(service.getBookTitlesByPublisher("TechMax"));
+		}, () -> "Performance issues with getBookTitlesByPublisher() method !");
 		assertEquals(10000, actualTitles.size());
 	}
 }
