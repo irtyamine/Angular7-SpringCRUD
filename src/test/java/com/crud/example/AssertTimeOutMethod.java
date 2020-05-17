5b8e87232c4951360c549873203ed238761d906f
Balmukund SINGH <balmukund.singh241@gmail.com>
2020-05-17 17:46:09 +0530

    Section-6/Assert Timeout with custom message done.

--- src/test/java/com/crud/example/AssertTimeOutMethod.java
+++ src/test/java/com/crud/example/AssertTimeOutMethod.java
@@ -13,7 +13,6 @@ class AssertTimeOutMethod {
 
 	@Test
 	void assert_timeout_without_messages() {
-		
 		BookService service = new BookService();
 		for(int i=1; i<=10000; i++) {
 			service.addBookInList(new BookModel(String.valueOf(i),"Core Java","TechMax" ));
@@ -23,7 +22,18 @@ class AssertTimeOutMethod {
 			actualTitles.addAll(service.getBookTitlesByPublisher("TechMax"));
 		});
 		assertEquals(10000, actualTitles.size());
-
 	}
 
+	@Test
+	void assert_timeout_with_messages() {
+		BookService service = new BookService();
+		for(int i=1; i<=10000; i++) {
+			service.addBookInList(new BookModel(String.valueOf(i),"Core Java","TechMax" ));
+		}
+		List<String> actualTitles = new ArrayList<>();
+		assertTimeout(Duration.ofMillis(1), ()-> {
+			actualTitles.addAll(service.getBookTitlesByPublisher("TechMax"));
+		},"Performance issues with getBookTitlesByPublisher() method !");
+		assertEquals(10000, actualTitles.size());
+	}
 }
